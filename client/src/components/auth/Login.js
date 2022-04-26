a1ea96f002c39e3b9893bf022c1da09c6c2ccd9a
Enrique Elizondo <quiqueev@Enriques-MacBook-Pro.local>
2022-04-26 17:47:27 -0500

    Antd, login front

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,88 @@
+import React from 'react'
+
+import { Form, Input, Button, Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const Login = () => {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+      };
+    
+      const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
+
+    const LoginForm = () => (
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+      
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+      
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        );
+
+  return (
+        <div>
+            <Layout style={{height:"100vh"}}>
+                <Header>
+                    <h1>CodeKraft</h1>
+                </Header>
+                <Content>
+                    <br />
+                    <LoginForm />
+                </Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </div>
+  )
+}
+
+export default Login
