55d32cbf7048c454a0b023e2923e78bd26cb6aba
Enrique Elizondo <quiqueev@Enriques-MacBook-Pro.local>
2022-05-05 18:51:44 -0500

    Added backend route to show react BUILD

--- server/app.js
+++ server/app.js
@@ -10,17 +10,21 @@ connectDB(process.env.MONGO_URI)
 
 const app = express()
 
+// MIDDLEWEAR
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "../client", "build")))
+app.use(express.static("public"))
 
 // AUTH USER
 app.use('/api/auth', require('./routes/auth'))
 
 const port = process.env.PORT || 3001
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
+  
 
 const server = app.listen(port, () => {
     console.log('Codekraft port: ' + port)
