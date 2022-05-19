b7acc6eb0a56849b77456faa8edbc6703f8332a8
Enrique Elizondo <quiqueev@Enriques-MacBook-Pro.local>
2022-05-19 15:29:10 -0500

    Added pdf to viewfile

--- client/src/components/general/ViewFile.js
+++ client/src/components/general/ViewFile.js
@@ -3,9 +3,12 @@ import { useParams, useNavigate } from "react-router-dom"
 import UserContext from '../../contexts/UserContext'
 import HomeLayout from '../layout/Layout'
 import axios from 'axios'
-import { Spin } from 'antd'
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { Spin, Row, Col} from 'antd'
+import { Document, Page, pdfjs } from "react-pdf"
+import { getDownloadURL, ref } from 'firebase/storage'
+import { storage } from '../../firebase'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
 
 const ViewFile = () => {
     
@@ -21,28 +24,50 @@ const ViewFile = () => {
     }, [])
 
     const fetchDocument = async () => {
+
         const { data } = await axios.get(`/api/documents/documentByID/${id}`, config)
         setFile(`${data.document.file}`)
-        // setFile('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf')
+        
+        getDownloadURL(ref(storage, `documents/${data.document.fileName}`))
+        .then((url) => {
+            const xhr = new XMLHttpRequest()
+            xhr.responseType = 'blob'
+
+            console.log(url)
+
+            xhr.onload = (event) => {
+                const blob = xhr.response
+                setFile(blob)
+                console.log(blob)
+            }
+            xhr.open('GET', url)
+            xhr.send()
+
+            setFile(url)
+
+            const doc = document.getElementById('myPdf')
+            doc.setAttribute('src', url)
+
+        })
+        .catch((error) => {
+            // Handle any errors
+        })
+
         setLoading(false)
     }
-
-    // file={{ url: file, httpHeaders: { 'X-CustomHeader': '40359820958024350238508234', "Access-Control-Allow-Origin": "*", crossOrigin: "anonymous" } }}
-
-    const ShowPDF = () => (
-        <Document 
-            file={{ url: file, httpHeaders: {"Access-Control-Allow-Origin": "*"} }}
-        />
-    )
 
   return (
    <HomeLayout>
        <div>
-           {
-               loading
-               ? <Spin/>
-               : <ShowPDF/>
-           }
+           <Row justify="center" >
+               <Col align="center" >
+                {
+                    loading
+                    ? <Spin/>
+                    : <embed id="myPdf" width="800px" height="2100px" />
+                }
+               </Col>
+           </Row>
        </div>
    </HomeLayout>
   )
